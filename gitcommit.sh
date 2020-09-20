ef76dfccf5d0b3ff38dace110356fece12ffac1b
sekhar76405 <ssjio5402@gmail.com>
2020-09-20 21:46:54 +0530

    2nd prog learnt the basics

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+echo "press 1 for same msg and 2 for different msg :"
+read choice
+if [ $choice == 1 ]
+then
+	git add .
+	value=$(<cache.txt)
+	git commit -m "${value}"
 
-git add .
-echo "Enter the msg: "
-read msg
-echo "${msg}" > cache.txt
-git commit -m "${msg}"
+else
+  	git add .
+	echo "Enter the msg: "
+	read msg
+	echo "${msg}" > cache.txt
+	git commit -m "${msg}"
+fi
+
